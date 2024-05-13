c43127c31207c686fae67448e2cb1290640b8cd6
S00235259 <S00235259@LAB-D2037-10>
2024-05-13 13:16:40 +0100

    creating db

---
+++ Db/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exam2024;
+using System.Data.Entity;
+
+namespace Db
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create database
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+
+            using (db)
+            {
+
+                //create sample data
+                Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+                Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+                Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+
+                //add sample data to db
+                db.Customers.Add(c1);
+                db.Customers.Add(c2);
+                db.Customers.Add(c3);
+
+                //save db
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Exam2024/Booking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace Exam2024
+{
+    public class Booking
+    {
+        public int BookingId {  get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfParticpants { get; set; }
+
+        public virtual List<Customer> Customer { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set;}
+        public string Name { get; set;}
+        public string ContactNumber { get; set;}
+
+    }
+
+    public class RestaurantData: DbContext
+    {
+        public RestaurantData(string databaseName):base (databaseName) { }
+        public DbSet<Customer> Customers { get; set;}
+        public DbSet<Booking> Bookings { get; set;}
+    }
+}
