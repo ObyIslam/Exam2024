0c9feec465d1880b00e8baed9e78018d8fa1811b
S00235259 <S00235259@LAB-D2037-10>
2024-05-13 13:19:11 +0100

    db changes

--- Db/Program.cs
+++ Db/Program.cs
@@ -18,19 +18,27 @@ namespace Db
 
             using (db)
             {
+                try
+                {
+                    //create sample data
+                    Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+                    Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+                    Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
 
-                //create sample data
-                Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
-                Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
-                Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+                    //add sample data to db
+                    db.Customers.Add(c1);
+                    db.Customers.Add(c2);
+                    db.Customers.Add(c3);
 
-                //add sample data to db
-                db.Customers.Add(c1);
-                db.Customers.Add(c2);
-                db.Customers.Add(c3);
+                    //save db
+                    db.SaveChanges();
 
-                //save db
-                db.SaveChanges();
+                    Console.WriteLine("db was created");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("db was not created" + ex.ToString());
+                }
             }
         }
     }
