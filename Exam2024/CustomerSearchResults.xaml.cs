2f8858b98c2d998924836aee055ffd541aa7fadb
Md Islam <S00235259@atu.ie>
2024-05-13 15:30:15 +0100

    final changes

---
+++ Exam2024/CustomerSearchResults.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Exam2024;
+
+namespace Exam2024
+{
+    /// <summary>
+    /// Interaction logic for CustomerSearchResults.xaml
+    /// </summary>
+    public partial class CustomerSearchResults : Window
+    {
+        public CustomerSearchResults()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+
+            var query = from c in db.Customers
+                        select c.Name;
+
+            var query2 = from c in db.Customers
+                         select c.ContactNumber;
+
+            var query3 = from c in db.Customers
+                         select c.CustomerId;
+
+            
+
+        }
+
+        private void createBooking_btn_Click(object sender, RoutedEventArgs e)
+        {
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+
+            using (db)
+            {
+                //get information from texboxes
+                Customer newCustomer = new Customer();
+                newCustomer.Name = tbx_Name.Text;
+                newCustomer.ContactNumber = tbx_contactNumber.Text;
+
+                Booking newBooking = new Booking();
+                newBooking.BookingDate = (DateTime)dateChosen.SelectedDate;
+                newBooking.NumberOfParticpants = int.Parse(tbx_NoOfCustomers.Text);
+
+
+                //add them to databse
+                db.Customers.Add(newCustomer);  
+                db.Bookings.Add(newBooking);
+                
+
+                //save
+                db.SaveChanges();
+            }
+
+            MessageBox.Show("Your booking has been confirmed");
+
+        }
+    }
+}
--- Exam2024/MainWindow.xaml.cs
+++ Exam2024/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Exam2024
 {
     /// <summary>
@@ -23,6 +24,85 @@ namespace Exam2024
         public MainWindow()
         {
             InitializeComponent();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //load data with data box
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+
+            using (db)
+            {
+                var query5 = from b in db.Bookings
+                            select b.BookingDate;
+
+                var query2 = from c in db.Customers
+                             select c.Name + " " + c.ContactNumber;
+
+                var query3 = from b in db.Bookings
+                             select b.NumberOfParticpants;
+
+
+                if (query_datebox.SelectedDate.Value != null && query5.Contains(query_datebox.SelectedDate.Value))
+                {
+                    lbx_Customers.ItemsSource = query2.ToString() + query3.ToString();
+                }
+                else
+                {
+                    lbx_Customers.ItemsSource = "please select a date";
+                }
+
+            }
+
+            var query = from b in db.Bookings
+                            select b.BookingDate;
+            lbx_Customers.ItemsSource = query.ToString();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+
+            var query = from c in db.Customers
+                        select c.Name;
+
+            var query2 = from c in db.Customers
+                         select c.ContactNumber;
+
+            var query3 = from c in db.Customers
+                         select c.CustomerId;
+
+            //opens new window 
+            if (query.Contains(tbx_CustomerName.Text) && query2.Contains(tbx_ContactNumber.Text) && query3.Contains(int.Parse(tbx_NoOfCustomers.Text)))
+            {
+                openCustomerSearchWindow();
+                this.Close();
+            }
+
+
+        }
+
+        private void openCustomerSearchWindow()
+        {
+            var newWindow = new CustomerSearchResults();
+            newWindow.Show();
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            RestaurantData db = new RestaurantData("OODExam_ObyIslam");
+            if (lbx_Customers.SelectedItem!= null)
+            {
+                lbx_Customers.Items.Remove(lbx_Customers.SelectedItem);
+                //db.Bookings.Remove(lbx_Customers.SelectedItem)
+                lbx_Customers.ItemsSource = null;
+                lbx_Customers.ItemsSource = db.Customers;
+            }
+            
         }
     }
 }
